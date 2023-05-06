1950e366a803f3896681dc4325212173b2f4fd82
zobayerdev <zobayer.dev@gmail.com>
2023-05-06 21:51:01 +0600

    project create successful

--- app/src/main/java/com/trodev/quizpro/MainActivity.java
+++ app/src/main/java/com/trodev/quizpro/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // action bar title
+        getSupportActionBar().setTitle("your title");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 }
